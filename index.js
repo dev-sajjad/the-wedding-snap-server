21a0e8b9d5ec6958b51e4659b28b5fe5a33d6552
dev-sajjad <sajjadbmarpc@gmail.com>
2022-12-30 19:22:23 +0600

    database connection done, get limited and get all services data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
@@ -9,11 +10,68 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    return res.send('The wedding snap server is running.')
+
+const uri =
+    `mongodb+srv://${process.env.WEDDING_SNAP_USER}:${process.env.WEDDING_SNAP_PASS}@cluster0.ozga6sm.mongodb.net/?retryWrites=true&w=majority`;
+  
+const client = new MongoClient(uri);
+
+
+async function dbConnect() {
+    try {
+        await client.connect();
+        console.log('Database connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+dbConnect();
+
+const serviceConnection = client.db('weddingSnap').collection('services')
+
+
+// get limited services from database
+app.get('/', async(req, res) => {
+    try {
+        const query = {};
+        const cursor = serviceConnection.find(query).limit(3)
+        const services = await cursor.toArray();
+        res.send({
+            success: true,
+            message: "Successfully got the data.",
+            data: services
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+})
+
+// get all services data
+app.get('/services', async(req, res) => {
+    try {
+        const cursor = serviceConnection.find({});
+        const services = await cursor.toArray();
+        res.send({
+            success: true,
+            message: 'Successfully got the data.',
+            data: services
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
 })
 
 
+
+
+
+
 app.listen(port, (req, res) => {
-    console.log(`Wedding snap server is running on port ${port}`)
+    console.log(`Wedding snap server is running on port: ${port}`)
 })
